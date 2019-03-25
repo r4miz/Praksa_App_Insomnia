f9ed3b8df25162bdacb5e91aa0636c9afd8a81a7
marin000 <marinb997@gmail.com>
2019-03-25 12:22:16 +0100

    dodani za prije i nakon spavanja upiti

---
+++ kopbupitnik/src/app/nakon_spavanja/nakon.component.ts
@@ -0,0 +1,155 @@
+import {Component, OnInit} from "@angular/core";
+
+ @Component ({
+     templateUrl:'./nakon.component.html',
+     styleUrls: ['./nakon.component.css']
+
+ })
+
+ export class NakonComponent implements OnInit{
+    prvo: string = null;
+    drugo: number=null;
+    trece: string= null;
+    cetvrto: number=null;
+    peto: string= null;
+    sesto: number=null;
+    sedmo1: number=null;
+    sedmo2: number=null;
+    sedmo3: number=null;
+    sedmo4: number=null;
+    sedmo5: number=null;
+    osmo: number=null;
+    deveto: number=null;
+    deseto: number=null;
+    jedanaesto: number=null;
+    dvanaesto: string=null;
+    dvanaesto3: string=null;
+    trinaesto: string=null;
+    trinaesto3: string=null;
+    cetrnaesto: string=null;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  handleChange(e) {
+    var items = document.getElementsByTagName("li");
+   if (this.prvo != null){
+     items[0].setAttribute("style", "background-color: darkgreen;");
+   }
+   else {
+     items[0].removeAttribute("style");
+   }
+   if (this.drugo != null){
+    items[1].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[1].removeAttribute("style");
+  }
+  if (this.trece != null){
+    items[2].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[2].removeAttribute("style");
+  }
+  if (this.cetvrto != null){
+    items[3].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[3].removeAttribute("style");
+  }
+  if (this.peto != null){
+    items[4].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[4].removeAttribute("style");
+  }
+  if (this.sesto != null){
+    items[5].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[5].removeAttribute("style");
+  }
+  if (this.sedmo1 != null && this.sedmo2 != null && this.sedmo3 != null && this.sedmo4 != null && this.sedmo5 != null) {
+    items[6].setAttribute("style", "background-color: darkgreen;");
+  }
+  else if (this.sedmo1 != null || this.sedmo2 != null || this.sedmo3 != null && this.sedmo4 != null && this.sedmo5 != null) {
+    items[6].setAttribute("style", "background-color: darkorange;");
+  }
+  else {
+    items[6].removeAttribute("style");
+  }
+  if (this.osmo != null){
+    items[7].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[7].removeAttribute("style");
+  }
+  if (this.deveto != null){
+    items[8].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[8].removeAttribute("style");
+  }
+  if (this.deseto != null){
+    items[9].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[9].removeAttribute("style");
+  }
+  if (this.jedanaesto != null){
+    items[10].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[10].removeAttribute("style");
+  }
+  if(this.dvanaesto == "dvanaesto1"){
+      items[11].setAttribute("style","background-color: darkgreen;");
+  }
+  else if(this.dvanaesto == "dvanaesto2" && this.dvanaesto3!=null){
+    items[11].setAttribute("style","background-color: darkgreen;");
+  }
+  else if(this.dvanaesto == "dvanaesto2" && this.dvanaesto3==null){
+    items[11].setAttribute("style","background-color: darkorange;");
+  }
+  else{
+    items[11].removeAttribute("style");
+  }
+  if(this.trinaesto == "trinaesto1"){
+    items[12].setAttribute("style","background-color: darkgreen;");
+  }
+  else if(this.trinaesto == "trinaesto2" && this.trinaesto3!=null){
+    items[12].setAttribute("style","background-color: darkgreen;");
+  }
+  else if(this.trinaesto == "trinaesto2" && this.trinaesto3==null){
+    items[12].setAttribute("style","background-color: darkorange;");
+  }
+  else{
+    items[12].removeAttribute("style");
+  }
+  if (this.cetrnaesto != null){
+    items[13].setAttribute("style", "background-color: darkgreen;");
+  }
+  else {
+    items[13].removeAttribute("style");
+  }
+}
+
+checkDvanaesto(){
+  if(this.dvanaesto=="dvanaesto2"){
+    document.getElementById("dvanaestoCheck").hidden = false;
+  }
+  else{
+    document.getElementById("dvanaestoCheck").hidden = true;
+  }
+}
+checkTrinaesto(){
+  if(this.trinaesto=="trinaesto2"){
+    document.getElementById("trinaestoCheck").hidden = false;
+  }
+  else{
+    document.getElementById("trinaestoCheck").hidden = true;
+  }
+}
+ }
---
+++ kopbupitnik/src/app/prije_spavanja/prije.component.ts
@@ -0,0 +1,183 @@
+import {Component, OnInit} from "@angular/core";
+
+ @Component ({
+     templateUrl:'./prije.component.html',
+     styleUrls: ['./prije.component.css']
+ })
+
+ export class PrijeComponent implements OnInit{
+     prvo: string=null;
+     prvo3:string=null;
+     drugo:string=null;
+     trece:string=null;
+     trece1:string=null;
+     trece2:string=null;
+     cetvrto:string=null;
+     cetvrto1:string=null;
+     cetvrto2:string=null;
+     cetvrto3:string=null;
+     peto1:string=null;
+     peto2:string=null;
+     peto3:string=null;
+     sesto1:string=null;
+     sedmo1:string=null;
+     sedmo2:string=null;
+     osmo:string=null;
+     deveto:string=null;
+     deveto3:string=null;
+     deseto:number=null;
+     deseto8:string=null;
+     ddeseto:number=null;
+     jedanaesto:string=null;
+     dvanaesto:string=null;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  handleChange(e) {
+    var items = document.getElementsByTagName("li");
+    if(this.prvo == "prvo1"){
+        items[0].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.prvo == "prvo2" && this.prvo3!=null){
+        items[0].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.prvo == "prvo2" && this.prvo3==null){
+        items[0].setAttribute("style","background-color: darkorange;");
+      }
+      else{
+        items[0].removeAttribute("style");
+      }
+      if (this.drugo != null){
+        items[1].setAttribute("style", "background-color: darkgreen;");
+      }
+      else {
+        items[1].removeAttribute("style");
+      }
+      if(this.trece == "treceNe"){
+        items[2].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.trece == "treceDa"){
+        if(this.trece1==null || this.trece2==null){
+            items[2].setAttribute("style","background-color: darkorange;");
+        }
+        else{
+            items[2].setAttribute("style","background-color: darkgreen;");
+        }
+      }
+      else{
+        items[2].removeAttribute("style");
+      }
+      if(this.cetvrto == "cetvrtoNe"){
+        items[3].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.cetvrto == "cetvrtoDa"){
+        if(this.cetvrto1==null || this.cetvrto2==null || this.cetvrto3==null){
+            items[3].setAttribute("style","background-color: darkorange;");
+        }
+        else{
+            items[3].setAttribute("style","background-color: darkgreen;");
+        }
+      }
+      else{
+        items[3].removeAttribute("style");
+      }
+      if(this.peto1!=null && this.peto2!=null && this.peto3!=null)
+      {
+        items[4].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.peto1!=null || this.peto2!=null || this.peto3!=null){
+        items[4].setAttribute("style","background-color: darkorange;");
+      }
+      else{
+        items[4].removeAttribute("style");
+      }
+      if (this.sesto1 != null){
+        items[5].setAttribute("style", "background-color: darkgreen;");
+      }
+      else {
+        items[5].removeAttribute("style");
+      }
+      if(this.sedmo1!=null && this.sedmo2!=null){
+        items[6].setAttribute("style", "background-color: darkgreen;");
+      }
+      else if(this.sedmo1!=null || this.sedmo2!=null){
+        items[6].setAttribute("style", "background-color: darkorange;");
+      }
+      else{
+        items[6].removeAttribute("style");
+      }
+      if(this.osmo== "osmoNe" || this.osmo=="osmoDa"){
+        items[7].setAttribute("style", "background-color: darkgreen;");
+      }
+      else{
+        items[6].removeAttribute("style");
+      }
+      if(this.deveto == "devetoDa"){
+        items[8].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.deveto == "devetoNe" && this.deveto3!=null){
+        items[8].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.deveto == "devetoNe" && this.deveto3==null){
+        items[8].setAttribute("style","background-color: darkorange;");
+      }
+      else{
+        items[8].removeAttribute("style");
+      }
+      if(this.deseto!= null && this.deseto8!=null && this.ddeseto!=null){
+        items[9].setAttribute("style","background-color: darkgreen;");
+      }
+      else if(this.deseto!= null || this.deseto8!=null || this.ddeseto!=null){
+        items[9].setAttribute("style","background-color: darkorange;");
+      }
+      else{
+        items[9].removeAttribute("style");
+      }
+      if (this.jedanaesto != null){
+        items[10].setAttribute("style", "background-color: darkgreen;");
+      }
+      else {
+        items[10].removeAttribute("style");
+      }if (this.dvanaesto != null){
+        items[11].setAttribute("style", "background-color: darkgreen;");
+      }
+      else {
+        items[11].removeAttribute("style");
+      }
+  }
+  checkPrvo(){
+    if(this.prvo=="prvo2"){
+      document.getElementById("prvoCheck").hidden = false;
+    }
+    else{
+      document.getElementById("prvoCheck").hidden = true;
+    }
+  }
+  checkTrece(){
+    if(this.trece=="treceDa"){
+      document.getElementById("treceCheck").hidden = false;
+    }
+    else{
+      document.getElementById("treceCheck").hidden = true;
+    }
+  }
+  checkCetvrto(){
+    if(this.cetvrto=="cetvrtoDa"){
+      document.getElementById("cetvrtoCheck").hidden = false;
+    }
+    else{
+      document.getElementById("cetvroCheck").hidden = true;
+    }
+  }
+  checkDeveto(){
+    if(this.deveto=="devetoNe"){
+      document.getElementById("devetoCheck").hidden = false;
+    }
+    else{
+      document.getElementById("devetoCheck").hidden = true;
+    }
+  }
+ }
