4b2c647c6de932f78a646f8d8e1d4873835693f6
Bikicc <44501342+Bikicc@users.noreply.github.com>
2019-04-02 09:16:56 +0200

    Dodana klasa history related to sleep

---
+++ kopbupitnik/src/app/HistoryOfDiseasesRelatedToSleep.ts
@@ -0,0 +1,32 @@
+export class HistoryOfDiseasesRelatedToSleep {
+  HoursOfSleepPerNight: string = null;
+  TimeNeededToFallAsleep: number = null;
+  HaveDriverLicence: number = null;
+  selectedValueDriversLicenceCategory: string = null;
+  KmPerYear: number = null;
+  selectedValueSmoker: number = null;
+  QuantityOfAlcoholPerWeek: number = null;
+  selectedValueSleepDuringDay: number = null;
+  selectedValuesWhen: string = null;
+  HowLongDoYouSleepDuringDay: number = null;
+  NumberOfNapsPerWeek: number = null;
+  selectedValueRested: number = null;
+  selectedValueTimeOfDay: string = null;
+  selectedValueWakeUpDuringTheNight: number = null;
+  NumberOfTimes: number = null;
+  selectedValueHardToFallAsleepAgain: number = null;
+  selectedValueGetOutOfBed: number = null;
+  selectedValueWatchTV: number = null;
+  selectedValueWakeUpWithPain: number = null;
+  HowOften: number = null;
+  selectedValueFamilyIssueWithSleep: number = null;
+  Kinship: string = null;
+  Diagnosis: string = null;
+  selectedValueDiedInSleep: number = null;
+  selectedValueTreatment: number = null;
+  NameOfDisease: string[] = null;
+  Meds: string[] = null;
+  Dose: number[] = null;
+
+ // (onChange)="handleChange($event)
+}
